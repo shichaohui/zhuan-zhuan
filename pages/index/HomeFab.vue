c77d26e18cab19ec3d0a68efaaa150c6dcd0f066
石朝辉 <shichaohui@wakedata.com>
2022-04-08 19:33:22 +0800

    fix: 优化菜单交互

--- pages/index/HomeFab.vue
+++ pages/index/HomeFab.vue
@@ -6,30 +6,28 @@
         :pattern="fabPattern"
         :content="fabContent"
         @trigger="handleFabTrigger"
+        @fabClick="handleFabClick"
     />
+    <view v-if="fabExpanded" class="mask" @click="handleClickMask" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import themeConstants from '@/themes/constants'
 
-/**
- * 菜单组件引用
- */
-
+// 菜单组件引用
 const fabRef = ref()
 
-/**
- * 菜单样式
- */
+// 菜单是否已展开
+const fabExpanded = ref(false)
+
+// 菜单样式
 const fabPattern = {
     buttonColor: themeConstants.colorPrimary,
     selectedColor: themeConstants.colorPrimary,
 }
 
-/**
- * 菜单项
- */
+// 菜单项
 const fabContent = [
     {
         text: '上传',
@@ -51,13 +49,38 @@ const fabContent = [
     },
 ]
 
-/**
- * 触发菜单项
- */
+// 触发菜单项
 const handleFabTrigger = ({ index }) => {
     uni.navigateTo({ url: fabContent[index].page })
     fabRef.value.close()
 }
+
+// 点击菜单按钮
+const handleFabClick = () => {
+    fabExpanded.value = !fabExpanded.value
+}
+
+// 点击菜单遮罩
+const handleClickMask = event => {
+    if (!fabExpanded.value) {
+        return
+    }
+    // 阻止事件传递并关闭菜单
+    event.stopPropagation()
+    fabRef.value.close()
+    fabExpanded.value = false
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.mask {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+}
+</style>
